c625e7532f2aa4e8e3cec7881f65cfc85def91e2
fatherboard <rapiteu@hotmail.com>
2020-05-28 18:48:55 +0200

    Version final navbar?

--- includes/common/navbar.php
+++ includes/common/navbar.php
@@ -1,6 +1,29 @@
 <nav class="navbar">
     <ul class="navbar-nav">
 
+        <li class="logo">
+            <div class="branding">
+                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 202.43 157.66">
+                    <defs>
+                        <style>
+                            .cls-1 {
+                                fill: #ffb317;
+                            }
+                        </style>
+                    </defs>
+                    <title>Untitled-2</title>
+                    <path d="M54.08,74.72v1.56l0-1.56Z" transform="translate(-23.57 -48.24)" />
+                    <path d="M26.07,45.52Z" transform="translate(-23.57 -48.24)" />
+                    <path d="M56.66,17.68" transform="translate(-23.57 -48.24)" />
+                    <path d="M209.09,196.87l-16,2.18,1.83-12.33-.41.05a60.16,60.16,0,0,1-36,18.65c-15.59,2.12-32.75-1.95-26.52-28.74L141.44,136l1-3.85.56-2.4,12.84-55.59L73.05,85.39,65.3,118.74l.41-.06a52.14,52.14,0,0,1,15.23-11.53,60.28,60.28,0,0,1,19.36-6.43c13.29-1.81,33.15,1.59,27,27.88L116.79,174l-18,2.46,10.18-43.81c2.86-12.29-.63-21.89-17.69-19.56-11.65,1.58-22.38,9.15-27.42,17.16A18.22,18.22,0,0,0,61.05,137l-10.7,46-18,2.46,26-111.9,18-2.46L216.5,52.54,175.58,64.22,173.3,74l-5.23,22.5-17.51,75.36c-3.37,14.49.11,23.48,16.51,21.25a41.5,41.5,0,0,0,26.72-15.85,23.29,23.29,0,0,0,3.29-7.35L208,123.17,226,120.71l-12.89,55.47C211.28,184.06,209.83,191,209.09,196.87Z" transform="translate(-23.57 -48.24)" />
+                    <path class="cls-1" d="M200.35,192.57l-16,2.18,1.83-12.33-.41.05a60.16,60.16,0,0,1-36,18.65c-15.59,2.12-32.75-2-26.53-28.74l9.47-40.73,1-3.85.56-2.4,12.84-55.59L64.3,81.09l-7.75,33.35.41-.06A52.33,52.33,0,0,1,72.2,102.85a60.28,60.28,0,0,1,19.36-6.43c13.29-1.81,33.15,1.59,27,27.88l-10.55,45.38-18,2.46,10.18-43.81c2.86-12.29-.63-21.89-17.69-19.56-11.65,1.59-22.39,9.15-27.43,17.16a18.37,18.37,0,0,0-2.76,6.79l-10.7,46-18,2.46,26-111.9,18-2.45L207.76,48.24,166.84,59.92l-2.28,9.81-5.23,22.5-17.51,75.36c-3.37,14.5.1,23.48,16.51,21.25A41.46,41.46,0,0,0,185.05,173a23.52,23.52,0,0,0,3.29-7.35l10.87-46.77,18-2.46-12.89,55.47C202.54,179.76,201.09,186.67,200.35,192.57Z" transform="translate(-23.57 -48.24)" />
+                </svg>
+                <span class="link-text">hu</span>
+            </div>
+
+        </li>
+
+
         <li class="nav-item">
             <a href="index.php" class="nav-link">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" class="svg-inline--fa fa-home fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
@@ -49,28 +72,40 @@
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="users" class="svg-inline--fa fa-users fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                     <path fill="currentColor" d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"></path>
                 </svg>
-                <span class="link-text">Sobre Nosotros</span>
+                <span class="link-text">Sobre</span>
+                <span class="link-text">Nosotros</span>
 
             </a>
 
         </li>
 
-        <li class="nav-item">
-            <a href="perfil.php" class="nav-link">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="id-card" class="svg-inline--fa fa-id-card fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path fill="currentColor" d="M528 32H48C21.5 32 0 53.5 0 80v16h576V80c0-26.5-21.5-48-48-48zM0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V128H0v304zm352-232c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zm0 64c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zm0 64c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zM176 192c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zM67.1 396.2C75.5 370.5 99.6 352 128 352h8.2c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h8.2c28.4 0 52.5 18.5 60.9 44.2 3.2 9.9-5.2 19.8-15.6 19.8H82.7c-10.4 0-18.8-10-15.6-19.8z"></path>
+
+        <?php
+        if (isset($_SESSION["login"]) && $_SESSION["login"]) {
+            echo '<li class="nav-item">
+                <a href="perfil.php" class="nav-link">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="id-card" class="svg-inline--fa fa-id-card fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                        <path fill="currentColor" d="M528 32H48C21.5 32 0 53.5 0 80v16h576V80c0-26.5-21.5-48-48-48zM0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V128H0v304zm352-232c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zm0 64c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zm0 64c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zM176 192c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zM67.1 396.2C75.5 370.5 99.6 352 128 352h8.2c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h8.2c28.4 0 52.5 18.5 60.9 44.2 3.2 9.9-5.2 19.8-15.6 19.8H82.7c-10.4 0-18.8-10-15.6-19.8z"></path>
+                    </svg>
+                    <span class="link-text">Mi</span>
+                    <span class="link-text">Perfil</span>
+    
+                </a>
+            </li>';
+        } else {
+            echo '<li class="nav-item">
+            <a href="entrar.php" class="nav-link">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-in-alt" class="svg-inline--fa fa-sign-in-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M416 448h-84c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h84c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32h-84c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h84c53 0 96 43 96 96v192c0 53-43 96-96 96zm-47-201L201 79c-15-15-41-4.5-41 17v96H24c-13.3 0-24 10.7-24 24v96c0 13.3 10.7 24 24 24h136v96c0 21.5 26 32 41 17l168-168c9.3-9.4 9.3-24.6 0-34z"></path>
                 </svg>
-                <span class="link-text">Mi Perfil</span>
+                <span class="link-text">Login/Registro</span>
 
             </a>
 
-        </li>
+        </li>';
+        }
 
-
+        ?>
 
     </ul>
-
-
-
-
 </nav>
--- index.php
+++ index.php
@@ -11,47 +11,47 @@
     <main>
         <h1>Título</h1>
         <p><pre>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur commodo enim id varius.
-             Mauris dui nisl, semper vel rhoncus sed, commodo quis magna. Ut tempor, odio id tempus pellentesque, mauris
-              tellus blandit quam, nec venenatis purus nibh eu dui. Etiam aliquet leo ut eros faucibus pellentesque. 
-              Sed dui diam, gravida non luctus in, luctus et urna. Curabitur sed turpis sagittis, viverra ligula nec,
-               imperdiet leo. Maecenas at ultricies lectus. Vivamus ultrices mi nec semper accumsan.
+Mauris dui nisl, semper vel rhoncus sed, commodo quis magna. Ut tempor, odio id tempus pellentesque, mauris
+tellus blandit quam, nec venenatis purus nibh eu dui. Etiam aliquet leo ut eros faucibus pellentesque. 
+Sed dui diam, gravida non luctus in, luctus et urna. Curabitur sed turpis sagittis, viverra ligula nec,
+imperdiet leo. Maecenas at ultricies lectus. Vivamus ultrices mi nec semper accumsan.
 
 Nulla vel mauris at massa accumsan malesuada non sed lorem. Vestibulum condimentum, neque in 
 semper euismod, nibh ante fringilla ipsum, eget tristique ligula elit commodo enim. Sed 
 vulputate molestie varius. Nulla consequat et mi ac ultricies. Aenean ut magna erat. Morbi
- et nulla est. In pharetra, augue sed molestie tempor, dolor felis mollis nunc, at dignissim
-  magna ex eu odio. Donec sagittis leo eget commodo mattis. Nullam nisi ante, sollicitudin
-   in tellus ut, tempus scelerisque lorem. Vestibulum viverra, lacus quis rutrum vehicula,
-    est orci congue ante, at vulputate nisi neque nec risus. Nam eu augue eget tellus 
-    euismod rutrum. Quisque vulputate augue id elit finibus aliquam. Integer in consequat
-     diam, ac consectetur enim. Duis nisl ligula, malesuada vel varius sed, luctus feugiat
-      erat. In odio felis, dignissim nec nisl ac, efficitur porta nisi.
+et nulla est. In pharetra, augue sed molestie tempor, dolor felis mollis nunc, at dignissim
+magna ex eu odio. Donec sagittis leo eget commodo mattis. Nullam nisi ante, sollicitudin
+in tellus ut, tempus scelerisque lorem. Vestibulum viverra, lacus quis rutrum vehicula,
+est orci congue ante, at vulputate nisi neque nec risus. Nam eu augue eget tellus 
+euismod rutrum. Quisque vulputate augue id elit finibus aliquam. Integer in consequat
+diam, ac consectetur enim. Duis nisl ligula, malesuada vel varius sed, luctus feugiat
+erat. In odio felis, dignissim nec nisl ac, efficitur porta nisi.
 
 Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
- Morbi eget augue efficitur, porttitor massa et, faucibus nisi. Etiam ut elit tellus.
-  Nullam suscipit ac lectus in venenatis. Aliquam urna metus, maximus nec eleifend eu,
-   lobortis quis lorem. Nullam erat mi, pharetra euismod scelerisque a, cursus eu tellus.
-    Nunc feugiat ullamcorper aliquam. Morbi ac sem dignissim lectus convallis lobortis at
-     sed diam. Ut tristique in dui sit amet condimentum. Pellentesque et lectus ultrices,
-      consequat diam quis, viverra ligula. Suspendisse dapibus ullamcorper porta. Aliquam
-       vitae consequat quam.
+Morbi eget augue efficitur, porttitor massa et, faucibus nisi. Etiam ut elit tellus.
+Nullam suscipit ac lectus in venenatis. Aliquam urna metus, maximus nec eleifend eu,
+lobortis quis lorem. Nullam erat mi, pharetra euismod scelerisque a, cursus eu tellus.
+Nunc feugiat ullamcorper aliquam. Morbi ac sem dignissim lectus convallis lobortis at
+sed diam. Ut tristique in dui sit amet condimentum. Pellentesque et lectus ultrices,
+consequat diam quis, viverra ligula. Suspendisse dapibus ullamcorper porta. Aliquam
+vitae consequat quam.
 
 Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec
- sollicitudin ullamcorper enim, vel mattis lorem vulputate ut. Nulla nec erat ut nisl
-  tristique condimentum. Nulla erat elit, laoreet quis arcu varius, mattis egestas mi. In
-   pharetra maximus nulla, ac laoreet tellus mollis pharetra. Quisque pretium, neque quis
-    sodales luctus, augue ex laoreet mauris, ac vehicula orci magna in orci. Nulla convallis
-     ornare ante, vel imperdiet tellus sodales id. Curabitur faucibus in lectus in commodo.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et imperdiet ante.
+sollicitudin ullamcorper enim, vel mattis lorem vulputate ut. Nulla nec erat ut nisl
+tristique condimentum. Nulla erat elit, laoreet quis arcu varius, mattis egestas mi. In
+pharetra maximus nulla, ac laoreet tellus mollis pharetra. Quisque pretium, neque quis
+sodales luctus, augue ex laoreet mauris, ac vehicula orci magna in orci. Nulla convallis
+ornare ante, vel imperdiet tellus sodales id. Curabitur faucibus in lectus in commodo.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et imperdiet ante.
 
 Vivamus eu nibh ipsum. Ut quis nibh vel massa pellentesque eleifend. Integer imperdiet metus
- non ante consequat, ac congue dui ultrices. Sed vel tristique orci. Aliquam commodo semper
-  ultrices. Duis rhoncus egestas sem nec commodo. Nunc hendrerit mauris nec molestie euismod.
-   Proin facilisis, augue id mattis semper, turpis lorem consequat velit, ut venenatis libero
-    augue non mi. Morbi malesuada condimentum eleifend. Donec lobortis felis eget nisl luctus
-     dapibus aliquet in sem. Fusce at diam mi. Vivamus viverra sollicitudin diam sit amet
-      iaculis.</pre></p>
-    </main>
+non ante consequat, ac congue dui ultrices. Sed vel tristique orci. Aliquam commodo semper
+ultrices. Duis rhoncus egestas sem nec commodo. Nunc hendrerit mauris nec molestie euismod.
+Proin facilisis, augue id mattis semper, turpis lorem consequat velit, ut venenatis libero
+augue non mi. Morbi malesuada condimentum eleifend. Donec lobortis felis eget nisl luctus
+dapibus aliquet in sem. Fusce at diam mi. Vivamus viverra sollicitudin diam sit amet
+iaculis.</pre></p>
+</main>
 
 
 
